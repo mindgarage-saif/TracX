3616b9b55374afd969a7e95e507ac84d63d273c7
Saif Khan <saifkhichi96@gmail.com>
2024-12-05 19:10:39 +0100

    Remove unused skeleton classes

--- TracX/core/__init__.py
+++ TracX/core/__init__.py
@@ -1,13 +1,3 @@
 from .experiment import Experiment
-from .motion import MotionSequence
-from .skeletons import BaseSkeleton, Halpe26Skeleton, TheiaSkeleton
 
-__all__ = [
-    "Experiment",
-    "ExperimentMonocular",
-    "MotionSequence",
-    "estimation_2d_custom",
-    "BaseSkeleton",
-    "Halpe26Skeleton",
-    "TheiaSkeleton",
-]
+__all__ = ["Experiment"]
